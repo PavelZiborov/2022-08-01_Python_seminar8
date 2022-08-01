f6a03b42cec7b375d6eef6876ebadb016b36d3db
Pavel Ziborov <zzipp@inbox.ru>
2022-08-02 01:16:14 +0300

    добавил модуль добавления нового персонажа

---
+++ data_base/add.py
@@ -0,0 +1,31 @@
+import json
+
+# Добавление в существующий словарь новой записи вместе с id
+def NewElement(file_path, new_data): # где "file_path" это название файла который необходимо добавить в базу "data" (например 'people.json')
+    with open(file_path, 'r', encoding='utf8') as file:  # открываем файл на чтение
+        BD = json.load(file)
+
+        # ищем максимальный id среди существующих
+        id_number = 0
+        for i in range(0, len(BD)):
+            if BD[i]['id'] > id_number:
+                id_number = BD[i]['id']
+        new_data['id'] = id_number+1  # добавили следующий по возрастанию id
+
+        #добавление к словарю новый словарь
+        BD.append(new_data)
+        with open(file_path, 'w', encoding='utf8') as outfile:
+            json.dump(BD, outfile, ensure_ascii=False, indent=2)
+
+def NewPerson():
+    name = input()
+    phone = input()
+    return {'name': name, 'phone': phone}
+
+
+# new_data = {'name': 'Сидоров', 'phone': '+79033334455'}
+# NewElement('people.json',new_data)
+
+# data = [{'id': 1, 'name': 'Сидоров', 'phone': '+79033334455'}]
+# with open('people.json', 'w', encoding='utf8') as file:
+#     json.dump(data, file, ensure_ascii=False, indent=4)
